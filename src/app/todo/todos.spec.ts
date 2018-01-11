3e22b3c22b355e4989d33fb4ad252f76a7a4c930
Krzysztof Jelski <krzysztof.jelski@pragmatists.pl>
2018-01-11 14:32:04 +0100

    Refactor tests a bit.

--- src/app/todo/todos.spec.ts
+++ src/app/todo/todos.spec.ts
@@ -1,12 +1,12 @@
-import test, {App, expectThat, http, Req, Server} from 'ng-test-runner';
+import test, {App, expectThat, Fixture, http, Req, Server} from 'ng-test-runner';
 import {async} from '@angular/core/testing';
 import {TodosComponent, TodosModule} from "./todos";
-import {By} from "@angular/platform-browser";
 
 describe('Todos component', () => {
 
     let app: App;
     let server: Server;
+    let comp: Fixture;
 
     beforeEach(async(() => {
         app = test(TodosModule);
@@ -35,7 +35,7 @@ describe('Todos component', () => {
 
     describe("with running server", () => {
 
-        it('shows the number of todos left', async(() => {
+        beforeEach(async(()=>{
             server.get('http://localhost:3000/todos', (req: Req) => {
                 req.sendJson(
                     [
@@ -44,18 +44,19 @@ describe('Todos component', () => {
                     ]
                 )
             });
+            comp = app.run(TodosComponent);
+        }));
 
-            const comp = app.run(TodosComponent);
-
+        it('shows the number of todos left', async(() => {
             comp.verify(
                 expectThat.textOf('[data-todos-count]').isEqualTo('Wow! Only 2 todos left!'),
-                (fixture) => {
-                    expect(fixture.debugElement.query(By.css('todo-list')).componentInstance.todos).toEqual(                    [
-                            {id: 1, title: 'Walk the first dog', completed: false},
-                            {id: 2, title: 'Walk the second dog', completed: false},
-                        ]
-                    ) }
+            )
 
+        }));
+
+        it('shows the todo items', async(() => {
+            comp.verify(
+                expectThat.textsOf('todo-list [data-todo-title]').areEqualTo(['Walk the first dog', 'Walk the second dog'] )
             )
 
         }));
